7a2fe67eed78a11c2bdb1a3db8125a1dfde7c2e2
Aiden <aidenwalsh2020@gmail.com>
2020-12-18 17:44:20 -0600

    automatic category tag on posts

--- post-board-backend/post-board/routes/posts.js
+++ post-board-backend/post-board/routes/posts.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var posts = require('../models/posts');
+var categories = require('../models/categories');
+
 
 router.post('/post', (req, res, next) => {
     console.log(req.body);
@@ -12,6 +14,36 @@ router.post('/post', (req, res, next) => {
           header: req.body.header,
           content: req.body.content,
           category: req.body.category
+        });
+        newPost.save(function(err) {
+          if (err) {
+            console.log(err);
+            res.json({
+              status: 400,
+              message: "error creating post"
+            });
+          } else {
+            console.log(newPost);
+            res.json({
+              status: 200, 
+              message: 'post created'
+            });
+          };
+        });
+      });
+  });
+
+  //automatically set the category when you are making a post on a category page
+  router.post('/:category', (req, res, next) => {
+    console.log(req.body);
+    posts.findOne({
+      header: req.body.header,
+    })
+    .then(post => {
+        let newPost = new posts({
+          header: req.body.header,
+          content: req.body.content,
+          category: (req.params.category)
         });
         newPost.save(function(err) {
           if (err) {
