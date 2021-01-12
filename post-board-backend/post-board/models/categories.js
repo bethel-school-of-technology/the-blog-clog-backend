52afd1208ee8bf2edad91f003bc90981cea2f6c4
Kateleigh <klorimer21@gmail.com>
2021-01-11 19:35:04 -0500

    Auto stash before merge of "main" and "origin/main"

--- post-board-backend/post-board/models/categories.js
+++ post-board-backend/post-board/models/categories.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var categorySchema = new mongoose.Schema({
     title: String,
     description: String,
-   });
+});
 
 module.exports = mongoose.model('categories', categorySchema);
